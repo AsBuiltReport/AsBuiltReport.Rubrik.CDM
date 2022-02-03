26498bd9c906f52729b7241450ee929d0dde1a76
Mike Preston <mwpreston@gmail.com>
2022-02-03 09:27:13 -0500

    update param and required modules

--- Src/Public/Invoke-AsBuiltReport.Rubrik.CDM.ps1
+++ Src/Public/Invoke-AsBuiltReport.Rubrik.CDM.ps1
@@ -19,8 +19,7 @@ function Invoke-AsBuiltReport.Rubrik.CDM {
     param (
         [String[]] $Target,
         [PSCredential] $Credential,
-        [String] $Token,
-        [String]$StylePath
+        [String] $Token
     )
 
     # Import JSON Configuration for Options and InfoLevel
